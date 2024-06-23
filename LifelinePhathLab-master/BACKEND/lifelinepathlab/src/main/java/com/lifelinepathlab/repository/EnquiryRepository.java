2e7447df63ad2d9445cb4b78f87c16e1457959e3
Rohitgupta3031 <rohitgupta.20144@gmail.com>
2024-06-23 13:34:40 +0530

    charge

--- LifelinePhathLab-master/BACKEND/lifelinepathlab/src/main/java/com/lifelinepathlab/repository/EnquiryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.lifelinepathlab.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.lifelinepathlab.model.Enquiry;
-
-public interface EnquiryRepository extends JpaRepository<Enquiry, Integer> {
-
-}
